5f9f904d80479b4a76802b30918862ffa104ea70
temitopefa <lasbohoo@gmail.com>
2024-06-25 06:07:56 -0700

    basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 5
+
+#calculate the sum
+sum_result = number1 + number2
+
+# Calculate the difference
+difference_result = number1 - number2
+
+# Calculate the product
+product_result = number1 * number2
+
+# Print the results
+print(f"Sum of {number1} and {number2} is {sum_result}")
+print(f"Difference of {number1} and {number2} is {difference_result}")
+print(f"Product of {number1} and {number2} is {product_result}")
